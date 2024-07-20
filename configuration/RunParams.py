da91736c7a319d4dc125d300b243fff828d9d6a4
nathanbowness <nathanbowness@gmail.com>
2024-07-20 12:37:19 -0400

    reorganize the radar processing code to ready for the tracking and video data

---
+++ configuration/RunParams.py
@@ -0,0 +1,27 @@
+from .CFARParams import CFARParams
+from .CFARType import CfarType
+from .RunType import RunDataFormat, RunType
+
+class RunParams():
+    """
+    Parameters to run the radar system
+    """
+    def __init__(self, cfar_params: CFARParams):
+        # Settings to let the program run from recorded data should be recorded, or read from a preexisting location
+        self.recordedDataFolder= "test_data"
+        self.runType = RunType.LIVE
+        self.recordedDataFormat = RunDataFormat.FD_CUSTOM
+        
+        if (self.runType == RunType.LIVE_RECORD):
+          print("Data will be recorded.")
+        
+        if(self.recordedDataFormat == RunDataFormat.SENTOOL_FD and not self.runType == RunType.RERUN):
+            exit("You cannot use the SENTOOL_FD format for a live data run, since this program cannot record data in that format.")
+        
+        self.ramp_type = "UP-Ramp"
+        self.cfar_params = cfar_params
+        # The minimum amount of data that must be kept in memory to window on
+        if(self.cfar_params.cfar_type == CfarType.CASO):
+            self.data_window_size = cfar_params.num_guard + cfar_params.num_train + 1
+        else:
+            self.data_window_size = (2*cfar_params.num_guard) + (2*cfar_params.num_train) + 1
