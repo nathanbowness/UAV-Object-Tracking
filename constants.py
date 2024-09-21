2b2ba6690636482127dcfb7e1fb8975bb025a394
nathan.bowness <nathan.bowness@gmail.com>
2024-09-21 16:41:31 -0400

    Small change to check git access

--- constants.py
+++ constants.py
@@ -2,4 +2,4 @@ SPEED_LIGHT = 299792458  # Speed of light in m/s
 DIST_BETWEEN_ANTENNAS = 0.00625 # Distance between recievers of radar
 
 RADAR_DETECTION_TYPE = "radar"
-IMAGE_DETECTION_TYPE = "image"
+IMAGE_DETECTION_TYPE = "image" 
